daeca76ebc39c9d243ab9d2219e02a01381e7421
Josh Wilson <joshuef@gmail.com>
2023-06-27 11:57:27 +0900

    feat(networking): add external address if global

--- sn_networking/src/lib.rs
+++ sn_networking/src/lib.rs
@@ -106,6 +106,7 @@ pub struct SwarmDriver {
     replication_fetcher: ReplicationFetcher,
     local: bool,
     dialed_peers: CircularVec<PeerId>,
+    is_client: bool,
 }
 
 impl SwarmDriver {
@@ -359,6 +360,7 @@ impl SwarmDriver {
             replication_fetcher: Default::default(),
             local,
             dialed_peers: CircularVec::new(63),
+            is_client,
         };
 
         Ok((
